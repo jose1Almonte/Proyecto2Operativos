9e597634e71f7168cd9918642e4f5ba8cd736883
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-03-13 12:10:43 -0400

    clase Administrador con las listas

---
+++ src/Classes/Administrador.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+
+
+/**
+ *
+ * @author Hallo
+ */
+public class Administrador extends Thread{
+
+    public static LinkedList colaNivel1Jose = new LinkedList();
+    public static LinkedList colaNivel2Jose = new LinkedList();
+    public static LinkedList colaNivel3Jose = new LinkedList();
+
+    public static LinkedList colaNivel1Andy = new LinkedList();
+    public static LinkedList colaNivel2Andy = new LinkedList();
+    public static LinkedList colaNivel3Andy = new LinkedList();
+
+    public static LinkedList colaNivel1Useche = new LinkedList();
+    public static LinkedList colaNivel2Useche = new LinkedList();
+    public static LinkedList colaNivel3Useche = new LinkedList();
+    
+    @Override
+    public void run(){
+        
+        
+        
+        
+        
+    }
+}
